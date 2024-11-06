9c084ee12e7a312ef4817befc8eea6e5b921830a
trinteen <140308014+trinteen@users.noreply.github.com>
2024-11-06 13:10:51 +0100

    update

--- install.cmd
+++ install.cmd
@@ -8,11 +8,29 @@ wmic logicaldisk get name,volumename
 echo ===============================
 set /p _INSTALL_MEDIA_=select drive: 
 
+REM Type install file (WIM/ESD):
+if exist %_INSTALL_MEDIA_%\sources\install.esd (
+	set _TYPE_FILE_=esd
+) else (
+	set _TYPE_FILE_=wim
+)
+
+REM Language:
+echo.
+echo System language:
+echo ===============================
+echo 1. English
+echo 2. Czech
+echo 3. Slovak
+echo X. Default
+echo ===============================
+set /p _SYS_LANG_=Select language:
+
 REM Select index Windows Edition:
 echo.
 echo Choose Windows edition:
 echo ===============================
-dism /Get-WimInfo /WimFile:%_INSTALL_MEDIA_%\sources\install.esd
+dism /Get-WimInfo /WimFile:%_INSTALL_MEDIA_%\sources\install.%_TYPE_FILE_%
 echo ===============================
 set /p _INSTALL_EDITION_=Index edition: 
 
@@ -37,12 +55,33 @@ echo assign letter=C >> script.txt
 diskpart /s script.txt
 
 REM Install Windows to selected drive:
-dism /Apply-Image /ImageFile:%_INSTALL_MEDIA_%\sources\install.esd /index:%_INSTALL_EDITION_% /ApplyDir:C:
+dism /Apply-Image /ImageFile:%_INSTALL_MEDIA_%\sources\install.%_TYPE_FILE_% /index:%_INSTALL_EDITION_% /ApplyDir:C:
 
 REM Setup UEFI Boot:
 echo.
 C:\Windows\System32\bcdboot C:\Windows /s Z: /f UEFI
 
+REM Unattend.xml:
+If %_SYS_LANG_% == [] (
+	type unattend\header.txt unattend\footer.txt > unattend.xml
+) else (
+	if %_SYS_LANG_% == 1 (
+		type unattend\header.txt unattend\en.txt unattend\footer.txt > unattend.xml
+	)
+	if %_SYS_LANG_% == 2 (
+		type unattend\header.txt unattend\cz.txt unattend\footer.txt > unattend.xml
+	)
+	if %_SYS_LANG_% == 3 (
+		type unattend\header.txt unattend\sk.txt unattend\footer.txt > unattend.xml
+	)
+	if %_SYS_LANG_% == X (
+		type unattend\header.txt unattend\footer.txt > unattend.xml
+	)
+)
+
+mkdir C:\Windows\Panther
+copy unattend.xml C:\Windows\Panther
+
 REM Finish:
-echo.
-echo Finish! Reboot system please :) 
+C:\Windows\System32\shutdown.exe /r /t 0
+ 
